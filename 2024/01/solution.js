6c0b20b6e136af419894f6aa85a8bac72b26fb50
Luan-CortesZ <luan.corteszuka@gmail.com>
2024-12-01 23:15:05 +0100

    Clean solution

--- 2024/01/solution.js
+++ 2024/01/solution.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const fileContent = fs.readFileSync('input.txt', 'utf8').split('\n');
 
-const leftNumbers = [];
+const leftNumbers = []; 
 const rightNumbers = [];
 const resultsDistance = [];
 const resultsSimilaryScore = [];
@@ -13,37 +13,46 @@ fileContent.forEach((line) => {
     rightNumbers.push(parseInt(numbers[1].substring(0,5)));
 })
 
+//Sort the arrays
 leftNumbers.sort((a, b) => a - b);
 rightNumbers.sort((a, b) => a - b);
 
-function calculateDistanceBetweenNumbers(){
-    const results = [];
-    for (let i = 0; i < leftNumbers.length; i++) {
-        let result = leftNumbers[i]-rightNumbers[i];
-        if(result<0){{result = -result}}
-        results.push(result);
-    }
-    return results;
-};
-
-function calculateSimilaryScore(){
-    const results = [];
-    leftNumbers.forEach(left => {
-        let count = 0;
-        rightNumbers.forEach(right => {
-            if(left === right){
-                count++;
-            }
-        });
-        results.push(left*count);
-    });
-    return results;
-}
-
+//Calculate the distance between the numbers
 resultsDistance.push(...calculateDistanceBetweenNumbers());
 const sumDistance = resultsDistance.reduce((a, b) => a + b, 0);
 console.log(`Distance addition -> ${sumDistance}`);
 
+//Calculate the similary score
 resultsSimilaryScore.push(...calculateSimilaryScore());
 const sumSimilaryScore = resultsSimilaryScore.reduce((a, b) => a + b, 0);
 console.log(`Similary score addition -> ${sumSimilaryScore}`);
+
+/**
+ * Calculates the absolute distance between corresponding numbers in the leftNumbers and rightNumbers arrays.
+ * 
+ * @returns {number[]} An array of distances.
+ */
+function calculateDistanceBetweenNumbers(){
+    const results = [];
+    leftNumbers.forEach((left, index) => {
+        let result = Math.abs(left - rightNumbers[index]);
+        results.push(result);
+    });
+    
+    return results;
+};
+
+/**
+ * Calculates the similarity score for each number in the leftNumbers array.
+ * The similarity score is the product of the number and the count of its occurrences in the rightNumbers array.
+ * 
+ * @returns {number[]} An array of similarity scores.
+ */
+function calculateSimilaryScore(){
+    const results = [];
+    leftNumbers.forEach(left => {
+        let result = rightNumbers.filter(x => x === left).length * left;
+        results.push(result);
+    });
+    return results
+}
