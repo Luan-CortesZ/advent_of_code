15ffebc8518180dde04614d7f8ee5cbfd5721840
Luan-CortesZ <luan.corteszuka@gmail.com>
2024-12-01 22:24:15 +0100

    Day 1 - Part 2

--- 2024/01/solution.js
+++ 2024/01/solution.js
@@ -3,7 +3,8 @@ const fileContent = fs.readFileSync('input.txt', 'utf8').split('\n');
 
 const leftNumbers = [];
 const rightNumbers = [];
-const results = [];
+const resultsDistance = [];
+const resultsSimilaryScore = [];
 
 //Put the numbers in the left and right arrays
 fileContent.forEach((line) => {
@@ -15,15 +16,34 @@ fileContent.forEach((line) => {
 leftNumbers.sort((a, b) => a - b);
 rightNumbers.sort((a, b) => a - b);
 
-for (let i = 0; i < leftNumbers.length; i++) {
-    let result = leftNumbers[i]-rightNumbers[i];
-    if(result<0){{result = -result}}
-    results.push(result);
+function calculateDistanceBetweenNumbers(){
+    const results = [];
+    for (let i = 0; i < leftNumbers.length; i++) {
+        let result = leftNumbers[i]-rightNumbers[i];
+        if(result<0){{result = -result}}
+        results.push(result);
+    }
+    return results;
+};
 
-    console.log(leftNumbers[i], rightNumbers[i], result);
-    
+function calculateSimilaryScore(){
+    const results = [];
+    leftNumbers.forEach(left => {
+        let count = 0;
+        rightNumbers.forEach(right => {
+            if(left === right){
+                count++;
+            }
+        });
+        results.push(left*count);
+    });
+    return results;
 }
 
-const sum = results.reduce((a, b) => a + b, 0);
+resultsDistance.push(...calculateDistanceBetweenNumbers());
+const sumDistance = resultsDistance.reduce((a, b) => a + b, 0);
+console.log(`Distance addition -> ${sumDistance}`);
 
-console.log(sum);
+resultsSimilaryScore.push(...calculateSimilaryScore());
+const sumSimilaryScore = resultsSimilaryScore.reduce((a, b) => a + b, 0);
+console.log(`Similary score addition -> ${sumSimilaryScore}`);
